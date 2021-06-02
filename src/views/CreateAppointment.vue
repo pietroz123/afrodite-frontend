eccf49380f56578da1f07f97bb2717c6d5a37647
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2021-06-01 21:04:41 -0300

    Componente de agendamento virou uma view
    Co-authored-by: bgsrd@outlook.com <bgsrd@outlook.com>

---
+++ src/views/CreateAppointment.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="">
+        <NavBar />
+    </div>
+    <div>
+        <Steps :model="items" />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from "@/components/NavBar.vue";
+import Steps from 'primevue/steps';
+
+export default {
+    name: "Home",
+    components: {
+        NavBar,
+		Steps,
+    },
+    data() {
+        return {
+            items: [
+                {
+                    label: "Personal",
+                    to: "/steps",
+                },
+                {
+                    label: "Seat",
+                    to: "/steps/seat",
+                },
+                {
+                    label: "Payment",
+                    to: "/steps/payment",
+                },
+                {
+                    label: "Confirmation",
+                    to: "/steps/confirmation",
+                },
+            ],
+        };
+    },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,17 @@
 <template>
-  <div class="">
-    <NavBar />
-  </div>
+    <div class="">
+        <NavBar />
+    </div>
 </template>
 
 <script>
-
 // @ is an alias to /src
-import CreateAppointment from "@/components/CreateAppointment.vue";
 import NavBar from "@/components/NavBar.vue";
 
 export default {
-  name: "Home",
-  components: {
-    NavBar,
-    CreateAppointment,
-  },
+    name: "Home",
+    components: {
+        NavBar,
+    },
 };
 </script>
-
