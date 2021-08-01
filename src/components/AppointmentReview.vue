d9e07510bdc95d741813867d927f042550ba0400
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2021-08-01 20:52:27 -0300

    Moving appointment review to its own component

---
+++ src/components/AppointmentReview.vue
@@ -0,0 +1,85 @@
+<template>
+    <Card class="p-shadow-1">
+        <template #content>
+            <div class="p-d-flex p-jc-between">
+                <div class="p-d-flex p-flex-column p-ai-center c-w-100">
+                    <p class="p-m-0 p-text-bold p-pb-3">
+                        {{ serviceSelected.name }}
+                    </p>
+                    <p class="p-m-0">R${{ serviceSelected.af_preco__c }}</p>
+                    <p class="p-m-0">
+                        {{ serviceSelected.af_tempo_de_execucao_em_minutos__c }}
+                        min
+                    </p>
+                </div>
+
+                <Divider layout="vertical" />
+
+                <div class="p-d-flex p-flex-column p-ai-center c-w-100">
+                    <p class="p-m-0 p-text-bold p-pb-3">
+                        {{ professionalSelected.name }}
+                    </p>
+                    <div>
+                        <Avatar size="xlarge" shape="circle" class="p-mx-2">
+                            {{
+                                professionalSelected.name
+                                    .charAt(0)
+                                    .toUpperCase()
+                            }}
+                        </Avatar>
+                    </div>
+                </div>
+
+                <Divider layout="vertical" />
+
+                <div class="p-d-flex p-flex-column p-ai-center c-w-100">
+                    <p class="p-m-0 p-text-bold p-pb-3">
+                        {{ timeSelected.format("dddd - DD/MM/YYYY") }}
+                    </p>
+                    <p class="p-m-0">{{ timeSelected.format("HH:mm") }}</p>
+                </div>
+            </div>
+
+            <div class="p-mt-4">
+                <Button
+                    label="Confirmar agendamento"
+                    @click="scheduleAppointment"
+                    class="p-mt-3"
+                />
+            </div>
+        </template>
+    </Card>
+</template>
+
+<script>
+import Card from "primevue/card";
+import Divider from "primevue/divider";
+import Avatar from "primevue/avatar";
+import Button from "primevue/button";
+
+export default {
+    components: {
+        Card,
+        Divider,
+        Avatar,
+        Button,
+    },
+    props: {
+        serviceSelected: Object,
+        professionalSelected: Object,
+        timeSelected: Object,
+    },
+    methods: {
+        /**
+         * Appointment confirmation
+         */
+        scheduleAppointment() {
+            console.log(this.serviceSelected);
+            console.log(this.professionalSelected);
+            console.log(this.timeSelected);
+        },
+    },
+};
+</script>
+
+<style></style>
--- src/views/CreateAppointment.vue
+++ src/views/CreateAppointment.vue
@@ -55,56 +55,11 @@
     >
         <h2 class="c-appointment-divider">Resumo</h2>
 
-        <Card class="p-shadow-1">
-            <template #content>
-                <div class="p-d-flex">
-                    <div class="p-d-flex p-flex-column">
-                        <p class="p-m-0 p-text-bold p-pb-3">
-                            {{ serviceSelected.name }}
-                        </p>
-                        <p class="p-m-0">R${{ serviceSelected.af_preco__c }}</p>
-                        <p class="p-m-0">
-                            {{
-                                serviceSelected.af_tempo_de_execucao_em_minutos__c
-                            }}
-                            min
-                        </p>
-                    </div>
-
-                    <Divider layout="vertical" />
-
-                    <div class="p-d-flex p-flex-column p-ai-center">
-                        <p class="p-m-0 p-text-bold p-pb-3">
-                            {{ professionalSelected.name }}
-                        </p>
-                        <div>
-                            <Avatar size="xlarge" shape="circle" class="p-mx-2">
-                                {{
-                                    professionalSelected.name
-                                        .charAt(0)
-                                        .toUpperCase()
-                                }}
-                            </Avatar>
-                        </div>
-                    </div>
-
-                    <Divider layout="vertical" />
-
-                    <div class="p-d-flex p-flex-column">
-                        <p class="p-m-0 p-text-bold p-pb-3">
-                            {{ timeSelected.format("dddd - DD/MM/YYYY") }}
-                        </p>
-                        <p class="p-m-0">{{ timeSelected.format("HH:mm") }}</p>
-                    </div>
-                </div>
-
-                <Button
-                    label="Confirmar agendamento"
-                    @click="scheduleAppointment"
-                    class="p-mt-3"
-                />
-            </template>
-        </Card>
+        <AppointmentReview
+            :service-selected="serviceSelected"
+            :professional-selected="professionalSelected"
+            :time-selected="timeSelected"
+        />
     </div>
 
     <ScrollTop :threshold="200" />
@@ -116,13 +71,10 @@ import NavBar from "@/components/NavBar.vue";
 import SelectProfessional from "@/components/SelectProfessional.vue";
 import SelectService from "@/components/SelectService.vue";
 import SelectSchedule from "@/components/SelectSchedule.vue";
+import AppointmentReview from "@/components/AppointmentReview.vue";
 
 // import Steps from "primevue/steps";
-import Button from "primevue/button";
 import ScrollTop from "primevue/scrolltop";
-import Card from "primevue/card";
-import Divider from "primevue/divider";
-import Avatar from "primevue/avatar";
 
 export default {
     components: {
@@ -130,12 +82,9 @@ export default {
         SelectProfessional,
         SelectService,
         SelectSchedule,
+        AppointmentReview,
         // Steps,
-        Button,
         ScrollTop,
-        Card,
-        Divider,
-        Avatar,
     },
     data() {
         return {
@@ -189,14 +138,6 @@ export default {
             });
         },
         /**
-         * Appointment confirmation
-         */
-        scheduleAppointment() {
-            console.log(this.serviceSelected);
-            console.log(this.professionalSelected);
-            console.log(this.timeSelected);
-        },
-        /**
          * Handlers
          */
         handleProfessionalSelection(professionalSelected) {
@@ -216,6 +157,9 @@ export default {
 </script>
 
 <style lang="scss">
+.c-w-100 {
+    width: 100%;
+}
 .c-appointment-divider {
     padding-bottom: 1rem;
     &::after {
