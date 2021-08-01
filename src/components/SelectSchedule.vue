e783b771e40c4c6114808b67728082efbf808669
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2021-07-31 23:29:50 -0300

    - Added library for working with dates (dayjs) - Started implementing the available time slots

--- src/components/SelectSchedule.vue
+++ src/components/SelectSchedule.vue
@@ -3,13 +3,90 @@
 
     <p>{{ professional.name }}</p>
     <p>{{ service.name }}</p>
+
+    <div>
+        <Accordion>
+            <AccordionTab
+                v-for="timeSlot in timeSlots"
+                :header="timeSlot.day.format('dddd')"
+                :key="timeSlot.day.valueOf()"
+            >
+                <Button
+                    v-for="slot in timeSlot.slots"
+                    :key="slot.valueOf()"
+                    :label="slot.format('HH:mm')"
+                    class="p-mr-2"
+                />
+            </AccordionTab>
+        </Accordion>
+    </div>
 </template>
 
 <script>
+import Accordion from "primevue/accordion";
+import AccordionTab from "primevue/accordiontab";
+import Button from "primevue/button";
+import dayjs from "dayjs";
+require("dayjs/locale/pt-br");
+
 export default {
+    components: {
+        Accordion,
+        AccordionTab,
+        Button,
+    },
     props: {
         professional: Object,
         service: Object,
+    },
+    data() {
+        return {
+            timeSlots: [],
+        };
+    },
+    mounted() {
+        dayjs.locale("pt-br");
+        this.availableSlots();
+    },
+    methods: {
+        availableSlots() {
+            let availableDays = [];
+            let firstDate = dayjs(new Date(2021, 8 - 1, 2, 10)); // first slot
+            let currentDate = firstDate; // first slot
+            // console.log(currentDate);
+
+            for (let i = 0; i < 7; i++) {
+                let availableSlots = [];
+                currentDate = firstDate.add(i, "day");
+                console.log("dia: ", currentDate.format("dddd"));
+
+                let numSlots = 10;
+                let currentSlot = currentDate
+                    .set("hour", 10)
+                    .set("minute", 0)
+                    .set("second", 0);
+
+                for (let j = 0; j < numSlots; j++) {
+                    let newSlot = currentSlot.add(15, "minute");
+                    // console.log(newSlot);
+                    currentSlot = newSlot;
+                    availableSlots.push(currentSlot);
+                }
+
+                availableDays.push({
+                    day: currentDate,
+                    // day: currentDate.toDate(),
+                    // slots: availableSlots.map((dayjsInstance) =>
+                    //     dayjsInstance.toDate()
+                    // ),
+                    slots: availableSlots,
+                });
+            }
+
+            console.log("🚀 / availableDays", availableDays);
+
+            this.timeSlots = availableDays;
+        },
     },
 };
 </script>
