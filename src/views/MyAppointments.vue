81713c0cc83e8c6e45b0a84e4eaee22eb729e3ac
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2021-08-10 22:55:27 -0300

    Added fullcalendar library to show customer appointments

--- src/views/MyAppointments.vue
+++ src/views/MyAppointments.vue
@@ -5,30 +5,60 @@
     <div class="p-pt-3 p-px-6">
         <h2 class="c-main-divider">Meus Agendamentos</h2>
 
-        <ul v-for="ap in appointments" :key="ap.ag_sfid">
-            <li>{{ ap.ag_horario }}</li>
-            <li>{{ ap.pf_name }}</li>
-            <li>{{ ap.sv_name }}</li>
-        </ul>
+        <FullCalendar v-if="options.events.length" :options="options" />
     </div>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar.vue";
+import dayjs from "dayjs";
+require("dayjs/locale/pt-br");
+import "@fullcalendar/core";
+import FullCalendar from "@fullcalendar/vue3";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import brLocale from "@fullcalendar/core/locales/pt-br";
 
 export default {
     components: {
         NavBar,
+        FullCalendar,
     },
     data() {
         return {
+            options: {
+                locale: brLocale,
+                plugins: [
+                    dayGridPlugin,
+                    timeGridPlugin,
+                    interactionPlugin,
+                    listPlugin,
+                ],
+                headerToolbar: {
+                    left: "prev,next,today",
+                    center: "title",
+                    right: "listMonth,dayGridMonth,timeGridWeek,timeGridDay",
+                },
+                initialView: "listMonth",
+                editable: true,
+                selectable: true,
+                selectMirror: true,
+                dayMaxEvents: true,
+                events: [],
+            },
             appointments: [],
         };
     },
     mounted() {
+        dayjs.locale("pt-br");
         this.getAppointments();
     },
     methods: {
+        /**
+         * Recupera os agendamentos do cliente logado
+         */
         getAppointments() {
             let authUser = JSON.parse(localStorage.getItem("user"));
             let mobilePhone = authUser.af_telefone_celular__c;
@@ -40,10 +70,31 @@ export default {
                 .then((res) => {
                     let appointments = res.data.rows;
                     this.appointments = appointments;
+                    console.log("🚀 / appointments", appointments);
+
+                    this.createEventsFromAppointments(appointments);
                 });
+        },
+        /**
+         * Transforma os agendamentos em eventos para a biblioteca FullCalendar
+         */
+        createEventsFromAppointments(sfAppointments) {
+            let events = sfAppointments.map((app, i) => {
+                return {
+                    id: i,
+                    title: `${app.pf_name} - ${app.sv_name}`,
+                    start: new Date(app.ag_horario),
+                };
+            });
+            console.log("🚀 / events", events);
+            this.options.events = events;
         },
     },
 };
 </script>
 
-<style></style>
+<style>
+.fc {
+    max-height: calc(100vh - 200px);
+}
+</style>
